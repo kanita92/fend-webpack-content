2576d83dd148cecf2bc4cdccb2b49cfbcc88cb62
KanitaNiki <kanitaafrin@gmail.com>
2023-03-05 17:53:49 -0500

    install babel

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,8 @@
-alert("I exist!");
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+console.log(checkForName);
+console.log(handleSubmit);
+
+
+alert("I EXIST")
