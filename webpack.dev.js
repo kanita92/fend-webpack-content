c4de21bfab8ee00e2fb9d53a9d818b56a28b2385
KanitaNiki <kanitaafrin@gmail.com>
2023-03-06 20:58:36 -0500

    mode setup

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,8 @@ const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
+    mode:'development',
+    devtool:'source-map',
     entry: './src/client/index.js',
     module: {
         rules:[
@@ -21,7 +23,7 @@ module.exports = {
         plugins:[
             new HtmlWebPackPlugin({
                 template:"./src/client/views/index.html",
-                filename: "./index.html",
+                filename: "./index.html"
             })
         ]
         
